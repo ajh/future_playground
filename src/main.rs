bbce77239b034471796604d49c3f377e8b125de7
Andy Hartford <hartforda@gmail.com>
2016-11-12 17:09:40 -0500

    now its parallel

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
 extern crate futures;
+extern crate futures_cpupool;
 extern crate tokio_core;
 
 use futures::*;
 use futures::stream::Stream;
-use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
 use tokio_core::reactor::Core;
 
 fn main() {
-    let mut core = Core::new().unwrap();
+    let stdin = std::io::stdin();
+    let lock = stdin.lock();
 
-    let f = File::open("README.md").unwrap();
-    let reader = BufReader::new(f);
+    let lines = stream::iter(lock.lines());
 
-    let lines = stream::iter(reader.lines());
+    let mut pool_builder = futures_cpupool::Builder::new();
+    let pool = pool_builder.create();
 
-    let uplines = lines.map(|l| {
-        println!("{}", l.to_uppercase());
-        finished(())
+    let workers = lines.map(|l| {
+        let future = finished::<String, ()>(l)
+            .map(|l| l.to_uppercase())
+            .and_then(|l| {
+                println!("{}", l);
+                finished(())
+            });
+
+        pool.spawn(future)
     });
 
+    let mut core = Core::new().unwrap();
     let handle = core.handle();
-    let server = uplines.for_each(|future| {
+
+    let server = workers.for_each(|future| {
         handle.spawn(future);
         Ok(())
     });
 
-    // start core
     core.run(server).unwrap();
 }
