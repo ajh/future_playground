a0d861cb78c3b3615089a10a551fe3dbd3fb14f3
Andy Hartford <hartforda@gmail.com>
2016-11-12 16:08:28 -0500

    Maybe parallel??

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,22 @@ use tokio_core::reactor::Core;
 fn main() {
     let mut core = Core::new().unwrap();
 
-    // open file
     let f = File::open("README.md").unwrap();
     let reader = BufReader::new(f);
 
-    // convert to stream
-    // reader.lines() is std::io::Lines<std::io::BufReader<std::fs::File>>
     let lines = stream::iter(reader.lines());
-    // lines is futures::stream::IterStream<std::io::Lines<std::io::BufReader<std::fs::File>>>
 
-    // for lines, upcase them and print them
-    let print_upcased_lines = lines.and_then(|line| {
-        println!("{}", line.to_uppercase());
+    let uplines = lines.map(|l| {
+        println!("{}", l.to_uppercase());
         finished(())
     });
 
-    let future = print_upcased_lines.for_each(|_| Ok(()));
+    let handle = core.handle();
+    let server = uplines.for_each(|future| {
+        handle.spawn(future);
+        Ok(())
+    });
 
     // start core
-    core.run(future).unwrap();
+    core.run(server).unwrap();
 }
