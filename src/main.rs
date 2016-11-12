d697b65f7b59fd050875002e7cc84e98c726c2ac
Andy Hartford <hartforda@gmail.com>
2016-11-12 15:04:03 -0500

    Try to get simple file reading example to work

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,28 @@
 extern crate futures;
 extern crate tokio_core;
 
-use std::env;
-use std::net::SocketAddr;
-
-use futures::Future;
+use futures::*;
 use futures::stream::Stream;
-use tokio_core::io::{copy, Io};
-use tokio_core::net::TcpListener;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use tokio_core::reactor::Core;
 
 fn main() {
-    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
-    let addr = addr.parse::<SocketAddr>().unwrap();
+    let mut core = Core::new().unwrap();
 
-    // Create the event loop that will drive this server
-    let mut l = Core::new().unwrap();
-    let handle = l.handle();
+    // open file
+    let mut f = File::open("README.md").unwrap();
+    let reader = BufReader::new(f);
 
-    // Create a TCP listener which will listen for incoming connections
-    let socket = TcpListener::bind(&addr, &handle).unwrap();
+    // convert to stream
+    let lines = stream::iter(reader.lines());
 
-    // Once we've got the TCP listener, inform that we have it
-    println!("Listening on: {}", addr);
-
-    // Pull out the stream of incoming connections and then for each new
-    // one spin up a new task copying data.
-    //
-    // We use the `io::copy` future to copy all data from the
-    // reading half onto the writing half.
-    let done = socket.incoming().for_each(|(socket, addr)| {
-        let pair = futures::lazy(|| Ok(socket.split()));
-        let amt = pair.and_then(|(reader, writer)| copy(reader, writer));
-
-        // Once all that is done we print out how much we wrote, and then
-        // critically we *spawn* this future which allows it to run
-        // concurrently with other connections.
-        handle.spawn(amt.then(move |result| {
-            println!("wrote {:?} bytes to {}", result, addr);
-            Ok(())
-        }));
-
-        Ok(())
+    // for lines, upcase them and print them
+    let print_upcased_lines = lines.and_then(|line| {
+        println!("{}", line.to_uppercase());
     });
 
-    // Execute our server (modeled as a future) and wait for it to
-    // complete.
-    l.run(done).unwrap();
+    // start core
+    core.run(print_upcased_lines).unwrap();
 }
